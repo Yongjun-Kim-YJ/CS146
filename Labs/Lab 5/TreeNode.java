0bac482cbe1cddcc13bc26eb36419c985f2d9276
Yongjun-Kim-YJ <yongjun.j.kim@gmail.com>
2024-03-26 23:39:11 -0700

    lab 5 java

--- Labs/Lab 5/TreeNode.java
+++ Labs/Lab 5/TreeNode.java
@@ -1,3 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {}
+
+    TreeNode(int val) { 
+        this.val = val; 
+    }
     
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+    
+    public boolean isValidBST(TreeNode root) {
+        // inorder traversal
+        List<Integer> inorder = new ArrayList<>();
+        inorderTraversal(root, inorder);
+        
+        // Check if sorted
+        for (int i=1; i<inorder.size(); i++) {
+            if (inorder.get(i) <= inorder.get(i-1)) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    private void inorderTraversal(TreeNode node, List<Integer> inorder) {
+        if (node == null) return;
+
+        inorderTraversal(node.left, inorder);
+        inorder.add(node.val);
+        inorderTraversal(node.right, inorder);
+    }
 }
