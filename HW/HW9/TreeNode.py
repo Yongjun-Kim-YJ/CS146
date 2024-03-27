08677660ff03618514df8a164a436a6610f8e43e
Yongjun-Kim-YJ <yongjun.j.kim@gmail.com>
2024-03-26 23:51:33 -0700

    HW9 py done

---
+++ HW/HW9/TreeNode.py
@@ -0,0 +1,15 @@
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def lowestCommonAncestor(root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+    while root:
+        if p.val < root.val and q.val < root.val:
+            root = root.left
+        elif p.val > root.val and q.val > root.val:
+            root = root.right
+        else:
+            return root
+    return None
